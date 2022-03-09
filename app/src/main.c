9b0c945b7c330d624f0d770701a08cab2806b4ec
Daan Janssen <daanjanssenbe@yahoo.com>
2022-03-09 22:08:55 +0100

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -1,15 +1,12 @@
 #include <stdint.h>
 #include <stm32l4xx.h>
 
-
-
 void delay(unsigned int n){
 	volatile unsigned int delay = n;
 	while (delay--);
 }
 
 int main(void) {
-
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
 
@@ -35,7 +32,6 @@ int main(void) {
 	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
 	GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
 
-
 	while (1) {
 		if (!(GPIOB->IDR  & GPIO_IDR_ID13)) {
 			GPIOC->ODR |= GPIO_ODR_OD13;
@@ -60,6 +56,4 @@ int main(void) {
 			GPIOB->ODR &= ~GPIO_ODR_OD9;
 		}
 	}
-
-
 }
