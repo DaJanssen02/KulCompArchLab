d5509ea0aa3d342a9541d31ccbae5f7783f9c068
Daan Janssen <daanjanssenbe@yahoo.com>
2022-03-25 11:18:00 +0100

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -1,54 +1,121 @@
-/**
- * @file main.c
- * @version 0.1
- *
- */
-
 #include <stdint.h>
 #include <stm32l4xx.h>
 
+int mux = 0;
+int temperatuur = 0; //123,5
 
 void delay(unsigned int n){
-	volatile unsigned int delay = n;
-	while (delay--);
+    volatile unsigned int delay = n;
+    while (delay--);
+}
+
+void multiplexer(){
+    if (mux == 1) {
+        GPIOA->ODR &= ~GPIO_ODR_OD8;             //00
+        GPIOA->ODR &= ~GPIO_ODR_OD15;
+        GPIOA->ODR &= ~GPIO_ODR_OD6;
+        seg7(temperatuur/100);
+    } else if (mux == 2) {
+        GPIOA->ODR |= GPIO_ODR_OD8;             //10
+        GPIOA->ODR &= ~GPIO_ODR_OD15;
+        GPIOA->ODR |= GPIO_ODR_OD6;
+        seg7(temperatuur/10);
+    } else if (mux == 3) {
+        GPIOA->ODR &= ~GPIO_ODR_OD8;             //01
+        GPIOA->ODR |= GPIO_ODR_OD15;
+        GPIOA->ODR &= ~GPIO_ODR_OD6;
+        seg7(temperatuur%10);
+    } else if (mux == 4) {
+        GPIOA->ODR |= GPIO_ODR_OD8;             //11
+        GPIOA->ODR |= GPIO_ODR_OD15;
+        GPIOA->ODR &= ~GPIO_ODR_OD6;
+        seg7(minuten%10);
+        mux = 0;
+    }
+}
+
+void seg7(int n){
+    //alle segmenten resetten
+    GPIOA->ODR &= ~(GPIO_ODR_OD7 | GPIO_ODR_OD5);
+    GPIOB->ODR &= ~(GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+
+
+    if (n != 1 && n != 4){							//Segment A
+        GPIOB->ODR |= GPIO_ODR_OD0;
+    }
+
+    if (n != 5 && n != 6){							//Segment B
+        GPIOA->ODR |= GPIO_ODR_OD7;
+    }
+
+    if (n != 2){									//Segment C
+        GPIOA->ODR |= GPIO_ODR_OD5;
+    }
+
+    if (n != 1 && n != 4 && n != 7){ 				//Segment D
+        GPIOB->ODR |= GPIO_ODR_OD12;
+    }
+
+    if (n == 0 || n == 2 || n == 6 || n == 8){ 		//Segment E
+        GPIOB->ODR |= GPIO_ODR_OD15;
+    }
+
+    if (n != 1 && n != 2 && n != 3 && n != 7){ 		//Segment F
+        GPIOB->ODR |= GPIO_ODR_OD1;
+    }
+
+    if (n != 1 && n != 7 && n != 0){ 				//Segment G
+        GPIOB->ODR |= GPIO_ODR_OD2;
+    }
 }
 
 int main(void) {
+	​// Klok aanzetten
+	​RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;
 
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+	​// Klok selecteren, hier gebruiken we sysclk
+	​RCC->CCIPR &= ~RCC_CCIPR_ADCSEL_Msk;
+	​RCC->CCIPR |= RCC_CCIPR_ADCSEL_0 | RCC_CCIPR_ADCSEL_1;
 
-	GPIOB->MODER &= ~GPIO_MODER_MODE9_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE9_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT9;
+	​// Deep powerdown modus uitzetten
+	​ADC1->CR &= ~ADC_CR_DEEPPWD;
 
-	GPIOC->MODER &= ~GPIO_MODER_MODE13_Msk;
-	GPIOC->MODER |= GPIO_MODER_MODE13_0;
-	GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;
+	​// ADC voltage regulator aanzetten
+	​ADC1->CR |= ADC_CR_ADVREGEN;
+	​
+	delay_ms(4);
 
-	while (1) {
-		if ((GPIOB->IDR &= ~GPIO_MODER_MODE13_Msk) = 1) {
-			GPIOC->ODR |= GPIO_ODR_OD13;
-			GPIOB->ODR |= GPIO_ODR_OD9;
-			delay(5000);
-			GPIOC->ODR &= ~GPIO_ODR_OD13;
-			GPIOB->ODR &= ~GPIO_ODR_OD9;
-			delay(5000);
-		}
+	// Kalibratie starten
+	​ADC1->CR |= ADC_CR_ADCAL;
+	​while(ADC1->CR & ADC_CR_ADCAL);
 
-		else if ((GPIOB->IDR &= ~GPIO_MODER_MODE14_Msk) = 1){
-			GPIOC->ODR |= GPIO_ODR_OD13;
-			GPIOB->ODR |= GPIO_ODR_OD9;
-			delay(25000);
-			GPIOC->ODR &= ~GPIO_ODR_OD13;
-			GPIOB->ODR &= ~GPIO_ODR_OD9;
-			delay(25000);
-			}
+	​// ADC aanzetten
+	​ADC1->CR |= ADC_CR_ADEN;
 
-		else {
-			GPIOC->ODR |= GPIO_ODR_OD13;
-			GPIOB->ODR |= GPIO_ODR_OD9;
-		}
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+
+    // 7 segmenten aanzetten (als output declareren) en laag maken
+    GPIOA->MODER &= ~(GPIO_MODER_MODE7_Msk | GPIO_MODER_MODE5_Msk);
+    GPIOA->MODER |= (GPIO_MODER_MODE7_0 | GPIO_MODER_MODE5_0);
+    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT7 | GPIO_OTYPER_OT5);
+
+    GPIOB->MODER &= ~(GPIO_MODER_MODE0_Msk | GPIO_MODER_MODE12_Msk | GPIO_MODER_MODE15_Msk | GPIO_MODER_MODE1_Msk | GPIO_MODER_MODE2_Msk);
+    GPIOB->MODER |= (GPIO_MODER_MODE0_0 | GPIO_MODER_MODE12_0 | GPIO_MODER_MODE15_0 | GPIO_MODER_MODE1_0 | GPIO_MODER_MODE2_0);
+    GPIOB->OTYPER &= ~(GPIO_OTYPER_OT0 | GPIO_OTYPER_OT12 | GPIO_OTYPER_OT15 | GPIO_OTYPER_OT1 | GPIO_OTYPER_OT2);
+
+    //mux en punt aansturen
+    GPIOA->MODER &= ~(GPIO_MODER_MODE8_Msk | GPIO_MODER_MODE15_Msk | GPIO_MODER_MODE6_Msk);
+    GPIOA->MODER |= (GPIO_MODER_MODE8_0 | GPIO_MODER_MODE15_0 | GPIO_MODER_MODE6_0);
+    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT8 | GPIO_OTYPER_OT15 | GPIO_OTYPER_OT6);
+
+    //NTC
+	GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE0_0 | GPIO_MODER_MODE0_1;
+
+
+    while (1) {
+
 	}
-
-
 }
+
