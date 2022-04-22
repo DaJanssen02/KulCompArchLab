fcf8e965bb74aac44dbc8e6348573a0ca6263648
Daan Janssen <100412420+DaJanssen02@users.noreply.github.com>
2022-04-22 13:36:53 +0200

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -8,7 +8,7 @@ void delay(unsigned int n){
 }
 
 
-int temperatuur = 0;
+float temperatuur = 0;
 
 
 
@@ -43,6 +43,51 @@ int main(void) {
 	USART1->CR1 = USART_CR1_TE | USART_CR1_RE | USART_CR1_UE;
 
 
+	//Klok aanzetten
+	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;
+
+	//systick configureren en interupt aanzetten
+	SysTick_Config(48000);
+	NVIC_SetPriority(SysTick_IRQn, 128);
+	NVIC_EnableIRQ(SysTick_IRQn);
+
+	//Klok selecteren, hier gebruiken we sysclk
+	RCC->CCIPR &= ~RCC_CCIPR_ADCSEL_Msk;
+	RCC->CCIPR |= RCC_CCIPR_ADCSEL_0 | RCC_CCIPR_ADCSEL_1;
+
+
+	//Deep powerdown modus uitzetten
+	ADC1->CR &= ~ADC_CR_DEEPPWD;
+
+
+
+	//ADC voltage regulator aanzetten
+	ADC1->CR |= ADC_CR_ADVREGEN;
+
+	delay(2000);
+
+	//Kalibratie starten
+	ADC1->CR |= ADC_CR_ADCAL;
+	while(ADC1->CR & ADC_CR_ADCAL);
+
+	//ADC aanzetten
+	ADC1->CR |= ADC_CR_ADEN;
+
+
+
+	//kanalen instellen
+	ADC1->SMPR1 |= (ADC_SMPR1_SMP5_0 | ADC_SMPR1_SMP5_1 | ADC_SMPR1_SMP5_2); //111 traagste sample frequentie
+	ADC1->SQR1 &= ~(ADC_SQR1_L_0 | ADC_SQR1_L_1 | ADC_SQR1_L_2 | ADC_SQR1_L_3);
+
+
+
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+
+
+    //NTC
+	GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE0_0 | GPIO_MODER_MODE0_1;
 
 
 	ADC1->SMPR1 |= (ADC_SMPR1_SMP5_0 | ADC_SMPR1_SMP5_1 | ADC_SMPR1_SMP5_2); //111 traagste sample frequentie
@@ -52,7 +97,7 @@ int main(void) {
 	GPIOA->MODER |= GPIO_MODER_MODE0_0 | GPIO_MODER_MODE0_1;
 
 	while (1) {
-		printf("temperatuur %d\r\n",temperatuur);
+		printf("temperatuur %f\r\n",temperatuur);
 
 		ADC1->SQR1 |= (ADC_SQR1_SQ1_2 | ADC_SQR1_SQ1_0); //00101
 		ADC1->SQR1 &= ~(ADC_SQR1_SQ1_1|ADC_SQR1_SQ1_3|ADC_SQR1_SQ1_4);
@@ -61,7 +106,7 @@ int main(void) {
 		float inputweerstand = ADC1->DR;
 		float voltage = (inputweerstand*3.0f)/4096.0f;
 		float weerstand = (10000.0f*voltage)/(3.0f-voltage);
-		temperatuur = ((1.0f/((logf(weerstand/10000.0f)/3936.0f)+(1.0f/298.15f)))-273.15f)*100;
+		temperatuur = ((1.0f/((logf(weerstand/10000.0f)/3936.0f)+(1.0f/298.15f)))-273.15f);
 
 		delay(10000);
 
@@ -70,3 +115,12 @@ int main(void) {
 
 
 }
+
+
+
+
+void SysTick_Handler(void) {
+
+}
+
+
